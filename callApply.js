61aea2384f1197f7364bf2ad4faf73951bb89e4a
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-08-09 00:46:00 +0600

    bind

--- callApply.js
+++ callApply.js
@@ -13,6 +13,29 @@ const normalPerson = {
 }
 
 // normalPerson.getFullName();
-normalPerson.chargeBill(10000);
-normalPerson.chargeBill(20000);
-console.log(normalPerson.salary);
+// normalPerson.chargeBill(10000);
+// normalPerson.chargeBill(20000);
+// console.log(normalPerson.salary);
+
+const heroPerson = {
+    firsName: 'Hero',
+    lastName: 'Balam',
+    salary: 25000
+}
+
+const friendlyPerson = {
+    firsName: 'Hero',
+    lastName: 'Gulam',
+    salary: 15000
+}
+
+const heroParsonBill = normalPerson.chargeBill.bind(heroPerson);
+
+// console.log('heroParsonCharge:',heroParsonBill(5000));
+// console.log('heroParsonSalary:',heroPerson.salary);
+// console.log('heroParsonCharge:',heroParsonBill(5000));
+// console.log('heroParsonSalary:',heroPerson.salary);
+// console.log('normalParson:',normalPerson.salary);
+
+const friendlyPersonBill = normalPerson.chargeBill.bind(friendlyPerson);
+ console.log(friendlyPersonBill(5000))
